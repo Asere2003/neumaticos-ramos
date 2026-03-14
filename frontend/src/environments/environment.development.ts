3f2f5bc00972eaaf09419bd1443f95adb4310f63
Asere2023 <albertopg2003@yahoo.es>
2026-03-14 23:39:44 +0100

    fix: eliminar environments del repositorio

--- frontend/src/environments/environment.development.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production:  false,
-  apiUrl:      'http://localhost:3000/api/v1',        // Para el navegador
-  apiUrlServer:'http://nr_backend:3000/api/v1',
-  supabaseUrl: 'https://phcawyzxnspyoyhabjdt.supabase.co',       // Para SSR
-  supabaseKey: 'sb_publishable_0chNk3L4eVtx1pLDjjEBog_wQ1P1YwX',
-
-
-};
--- frontend/src/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production:  true,
-  apiUrl:      'http://localhost:3000/api/v1',        // Para el navegador
-  apiUrlServer:'http://nr_backend:3000/api/v1',       // Para SSR
-  supabaseUrl: 'https://phcawyzxnspyoyhabjdt.supabase.co',
-  supabaseKey: 'sb_publishable_0chNk3L4eVtx1pLDjjEBog_wQ1P1YwX',
-};
