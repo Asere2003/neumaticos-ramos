b1d6cc178e7de2ad23dc4a49d338f449abd8df53
Asere2023 <albertopg2003@yahoo.es>
2026-03-16 01:13:50 +0100

    url back

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production:  true,
-  apiUrl:      'https://neumaticos-ramos-production-09c7.up.railway.app',        // Para el navegador
-  apiUrlServer:'https://neumaticos-ramos-production-09c7.up.railway.app',       // Para SSR
+  apiUrl:      'https://neumaticos-ramos-production-09c7.up.railway.app/api/v1',        // Para el navegador
+  apiUrlServer:'https://neumaticos-ramos-production-09c7.up.railway.app/api/v1',       // Para SSR
   supabaseUrl: 'https://phcawyzxnspyoyhabjdt.supabase.co',
   supabaseKey: 'sb_publishable_0chNk3L4eVtx1pLDjjEBog_wQ1P1YwX',
 };
